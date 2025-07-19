f194f9602b99fcc14d30b57bb23af86c87bc661b
kuzmepaul <kuzmepaul@gmail.com>
2025-07-19 14:46:39 +0300

    added room

---
+++ Assets/Code/PlayerExitTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace JamSpace
+{
+    public sealed class PlayerExitTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private CharacterController characterController;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Finish"))
+            {
+                characterController.enabled = false;
+                var pos = characterController.transform.position;
+                characterController.transform.position = pos.WithX(-pos.x).WithZ(-pos.z);
+                characterController.enabled = true;
+            }
+        }
+    }
+}
